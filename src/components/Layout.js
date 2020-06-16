9a22c573a95dfddfd86c26ea33c75ded6b064f73
FabioOrlinski <fabio.orlinski@gmail.com>
2020-06-16 12:01:39 -0300

    Cambio el socketUrl en master para que funcione en heroku, en develop lo dejo igual para poder correrlo local

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,7 @@ import { USER_CONNECTED, LOGOUT, VERIFY_USER } from '../Events'
 import LoginForm from './LoginForm'
 import ChatContainer from './chats/ChatContainer'
 
-const socketUrl = "http://localhost:3231"
+const socketUrl = "/"
 
 const Layout = () => {
 	const [socket, setSocket ] = useState(null);
